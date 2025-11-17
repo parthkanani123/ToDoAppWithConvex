75be0cf85f169f2421fa144c84107446a988d58b
parthkanani123 <parthkanani40@gmail.com>
2025-11-17 16:49:08 +0530

    Added UI and Fetching data logic

--- ToDoAppWithConvex/ContentView.swift
+++ ToDoAppWithConvex/ContentView.swift
@@ -5,17 +5,54 @@
 //  Created by Parth Kanani on 14/11/25.
 //
 
+import Combine
+import ConvexMobile
 import SwiftUI
 
+struct TaskItem: Hashable, Decodable {
+    let _id: String
+    let isCompleted: Bool
+    let text: String
+}
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var tasks: [TaskItem] = []
+    
+    let client = ConvexClient(deploymentUrl: "http://127.0.0.1:3210")
+    
+    func fetchTasks() async {
+        do {
+            for try await tasks: [TaskItem] in client.subscribe(to: "tasks:get").values
+            {
+                self.tasks = tasks
+            }
+        } catch {
+            // handle error
+            print("Failed to fetch tasks with error: \(error)")
         }
-        .padding()
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(tasks, id: \.self) { task in
+                    HStack {
+                        Text(task.text)
+                        Spacer()
+                        
+                        if task.isCompleted {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundStyle(.green)
+                        }
+                    }
+                }
+            }
+            .task {
+                await fetchTasks()
+            }
+            .navigationTitle("Tasks")
+        }
     }
 }
 
